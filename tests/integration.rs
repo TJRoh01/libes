27ced51ab5dd9b27ce055d2695101bc291d2b2c3
Tomas Rohatynski <tjroh01@gmail.com>
2022-10-30 13:33:10 -0500

    implement ECIES-AEAD & ECIES-SYN

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,6 +1,6 @@
 use rand_core::OsRng;
 use libes;
-use libes::auth::HmacSha256;
+use libes::auth::{Aead, HmacSha256, Syn};
 use libes::Ecies;
 use libes::enc::XChaCha20Poly1305;
 use libes::key::X25519;
@@ -13,10 +13,41 @@ fn x25519_xchachapoly1305_hmacsha256() {
 
     let sk = x25519_dalek::StaticSecret::new(OsRng);
     let pk = x25519_dalek::PublicKey::from(&sk);
-    let enc = X25519XChaCha20Poly1305HmacSha256::new(pk);
 
-    let ciphertext = enc.encrypt_mac(LOREM_IPSUM);
-    let plaintext = X25519XChaCha20Poly1305HmacSha256::decrypt_mac(sk, ciphertext);
+    let enc = X25519XChaCha20Poly1305HmacSha256::new(pk);
+    let ciphertext = enc.encrypt(LOREM_IPSUM);
+
+    let plaintext = X25519XChaCha20Poly1305HmacSha256::decrypt(sk, ciphertext);
+
+    assert_eq!(LOREM_IPSUM, plaintext.as_slice());
+}
+
+#[test]
+fn x25519_xchachapoly1305_aead() {
+    type X25519XChaCha20Poly1305Aead = Ecies::<X25519, XChaCha20Poly1305, Aead>;
+
+    let sk = x25519_dalek::StaticSecret::new(OsRng);
+    let pk = x25519_dalek::PublicKey::from(&sk);
+
+    let enc = X25519XChaCha20Poly1305Aead::new(pk);
+    let ciphertext = enc.encrypt(LOREM_IPSUM);
+
+    let plaintext = X25519XChaCha20Poly1305Aead::decrypt(sk, ciphertext);
+
+    assert_eq!(LOREM_IPSUM, plaintext.as_slice());
+}
+
+#[test]
+fn x25519_xchachapoly1305_syn() {
+    type X25519XChaCha20Poly1305Syn = Ecies::<X25519, XChaCha20Poly1305, Syn>;
+
+    let sk = x25519_dalek::StaticSecret::new(OsRng);
+    let pk = x25519_dalek::PublicKey::from(&sk);
+
+    let enc = X25519XChaCha20Poly1305Syn::new(pk);
+    let ciphertext = enc.encrypt(LOREM_IPSUM);
+
+    let plaintext = X25519XChaCha20Poly1305Syn::decrypt(sk, ciphertext);
 
     assert_eq!(LOREM_IPSUM, plaintext.as_slice());
 }
