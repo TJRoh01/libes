974da0f82b4b4813c41961cd7f4de68e6ba90cb4
Tomas Rohatynski <tjroh01@gmail.com>
2022-11-02 01:13:50 -0500

    undo docs.rs attribute, does not seem to work

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -2,19 +2,15 @@
 
 pub(crate) mod generics;
 
-#[cfg_attr(doc_cfg, doc(cfg(feature = "ECIES-AEAD")))]
 #[cfg(feature = "ECIES-AEAD")]
 /// Marker for using the `ECIES-AEAD` variant for authentication
 pub struct Aead;
 
-#[cfg_attr(doc_cfg, doc(cfg(feature = "ECIES-SYN")))]
 #[cfg(feature = "ECIES-SYN")]
 /// Marker for using the `ECIES-SYN` variant for authentication
 pub struct Syn;
 
 #[cfg(all(feature = "ECIES-MAC", feature = "HMAC-SHA256"))]
 mod hmacsha256;
-
-#[cfg_attr(doc_cfg, doc(cfg(feature = "ECIES-MAC", feature = "HMAC-SHA256")))]
 #[cfg(all(feature = "ECIES-MAC", feature = "HMAC-SHA256"))]
 pub use hmacsha256::HmacSha256;
--- src/enc/mod.rs
+++ src/enc/mod.rs
@@ -4,7 +4,5 @@ pub(crate) mod generics;
 
 #[cfg(feature = "XChaCha20-Poly1305")]
 mod xchacha20poly1305;
-
-#[cfg_attr(doc_cfg, doc(cfg(feature = "XChaCha20-Poly1305")))]
 #[cfg(feature = "XChaCha20-Poly1305")]
 pub use xchacha20poly1305::XChaCha20Poly1305;
