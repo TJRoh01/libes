7ac557b0c84dba3434af7bd2e64631d04b5d2711
Tomas Rohatynski <45859222+TJRoh01@users.noreply.github.com>
2023-04-05 23:43:29 -0500

    use custom public key conversion trait to aid documentation

--- src/key/mod.rs
+++ src/key/mod.rs
@@ -1,7 +1,32 @@
+//! Markers for Elliptic Curve algorithms supported by `libes`
+
+use generics::Key;
+
+/// A value -> `PublicKey` conversion that consumes the input value. The opposite of [IntoPublicKey].
+pub trait PublicKeyFrom<T>: Key {
+    fn pk_from(x: T) -> Self;
+}
+
+/// A value -> `PublicKey` conversion that consumes the input value. The opposite of [PublicKeyFrom].
+pub trait IntoPublicKey<U: Key> {
+    fn into_pk(self) -> U;
+}
+
+impl<T, U> IntoPublicKey<U> for T
+    where
+        U: PublicKeyFrom<T>
+{
+    fn into_pk(self) -> U {
+        U::pk_from(self)
+    }
+}
+
+/// A value -> `SecretKey` conversion that consumes the input value. The opposite of [IntoSecretKey].
 pub trait SecretKeyFrom<T>: Key {
     fn sk_from(x: T) -> Self::SecretKey;
 }
 
+/// A value -> `SecretKey` conversion that consumes the input value. The opposite of [SecretKeyFrom].
 pub trait IntoSecretKey<U: Key> {
     fn into_sk(self) -> U::SecretKey;
 }
