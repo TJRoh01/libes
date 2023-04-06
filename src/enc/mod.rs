48ad3fb13102b3a607c7979765c67956f22b824c
Tomas Rohatynski <45859222+TJRoh01@users.noreply.github.com>
2023-04-05 23:43:29 -0500

    add support for ChaCha20-Poly1305 encryption algorithm

--- src/enc/mod.rs
+++ src/enc/mod.rs
@@ -2,6 +2,11 @@
 
 pub(crate) mod generics;
 
+#[cfg(feature = "ChaCha20-Poly1305")]
+mod chacha20poly1305;
+#[cfg(feature = "ChaCha20-Poly1305")]
+pub use self::chacha20poly1305::ChaCha20Poly1305;
+
 #[cfg(feature = "XChaCha20-Poly1305")]
 mod xchacha20poly1305;
 #[cfg(feature = "XChaCha20-Poly1305")]
