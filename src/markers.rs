60b205853c7d4a8a37706bc61d517448b409d516
Tomas Rohatynski <45859222+TJRoh01@users.noreply.github.com>
2023-04-05 23:43:29 -0500

    split algorithm support into encryption & decryption part to allow enabling them individually

--- src/markers.rs
+++ src/markers.rs
@@ -1,8 +1,25 @@
+//! Support marker traits
+
 #[cfg(feature = "ECIES-MAC")]
-pub trait EciesMacSupport {}
+/// Implementors of this trait support the `ECIES-MAC` encryption variant
+pub trait EciesMacEncryptionSupport {}
+
+#[cfg(feature = "ECIES-MAC")]
+/// Implementors of this trait support the `ECIES-MAC` decryption variant
+pub trait EciesMacDecryptionSupport {}
 
 #[cfg(feature = "ECIES-AEAD")]
-pub trait EciesAeadSupport {}
+/// Implementors of this trait support the `ECIES-AEAD` encryption variant
+pub trait EciesAeadEncryptionSupport {}
+
+#[cfg(feature = "ECIES-AEAD")]
+/// Implementors of this trait support the `ECIES-AEAD` decryption variant
+pub trait EciesAeadDecryptionSupport {}
 
 #[cfg(feature = "ECIES-SYN")]
-pub trait EciesSynSupport {}
+/// Implementors of this trait support the `ECIES-SYN` encryption variant
+pub trait EciesSynEncryptionSupport {}
+
+#[cfg(feature = "ECIES-SYN")]
+/// Implementors of this trait support the `ECIES-SYN` decryption variant
+pub trait EciesSynDecryptionSupport {}
